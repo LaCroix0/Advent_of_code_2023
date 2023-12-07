f92ffae4e1f3ee8376a7d416278af0000de507dc
Jakub Wiańczyk <jwianczyk@gmail.com>
2023-12-07 22:10:24 +0100

    completed day 6

--- Day_4/main.py
+++ Day_4/main.py
@@ -1,47 +1,47 @@
-from functools import reduce
-
-
-# part 1 solution
-def part_1(row: str):
-    row = row[row.index(':') + 1:]
-    row = row.strip('\n ')
-    row = row.split('|')
-    winning_numbers = row[0].split(' ')
-    winning_numbers = [int(x) for x in winning_numbers if x != '']
-    my_numbers = row[1].split(' ')
-    my_numbers = [int(x) for x in my_numbers if x != '']
-    correct = []
-    for num in my_numbers:
-        if num in winning_numbers:
-            correct.append(num)
-    # print(f'my numbers : {my_numbers}')
-    # print(f'winning numbers : {correct}')
-    # print(f'matching numbers : {correct}')
-    out = 2 ** (len(correct) - 1) if len(correct) > 0 else 0
-    return out
-
-
-# part 2 solution
-def part_2(row: str):
-    row = row[row.index(':') + 1:]
-    row = row.strip('\n ')
-    row = row.split('|')
-    winning_numbers = row[0].split(' ')
-    winning_numbers = [int(x) for x in winning_numbers if x != '']
-    my_numbers = row[1].split(' ')
-    my_numbers = [int(x) for x in my_numbers if x != '']
-    correct = []
-    for num in my_numbers:
-        if num in winning_numbers:
-            correct.append(num)
-    return len(correct)
-
-
-with open('input.txt', 'r') as file:
-    res = []
-    cards_dics = {}
-    for line in file:
-        res.append(part_1(line))
-
-    print(f'part 1 solution: {int(reduce(lambda a, b: a + b, res))}')
-    print(f'part 2 solution: ')
+# from functools import reduce
+#
+#
+# # part 1 solution
+# def part_1(row: str):
+#     row = row[row.index(':') + 1:]
+#     row = row.strip('\n ')
+#     row = row.split('|')
+#     winning_numbers = row[0].split(' ')
+#     winning_numbers = [int(x) for x in winning_numbers if x != '']
+#     my_numbers = row[1].split(' ')
+#     my_numbers = [int(x) for x in my_numbers if x != '']
+#     correct = []
+#     for num in my_numbers:
+#         if num in winning_numbers:
+#             correct.append(num)
+#     # print(f'my numbers : {my_numbers}')
+#     # print(f'winning numbers : {correct}')
+#     # print(f'matching numbers : {correct}')
+#     out = 2 ** (len(correct) - 1) if len(correct) > 0 else 0
+#     return out
+#
+#
+# # part 2 solution
+# def part_2(row: str):
+#     row = row[row.index(':') + 1:]
+#     row = row.strip('\n ')
+#     row = row.split('|')
+#     winning_numbers = row[0].split(' ')
+#     winning_numbers = [int(x) for x in winning_numbers if x != '']
+#     my_numbers = row[1].split(' ')
+#     my_numbers = [int(x) for x in my_numbers if x != '']
+#     correct = []
+#     for num in my_numbers:
+#         if num in winning_numbers:
+#             correct.append(num)
+#     return len(correct)
+#
+#
+# with open('input.txt', 'r') as file:
+#     res = []
+#     cards_dics = {}
+#     for line in file:
+#         res.append(part_1(line))
+#
+#     print(f'part 1 solution: {int(reduce(lambda a, b: a + b, res))}')
+#     print(f'part 2 solution: ')
---
+++ Day_6/main.py
@@ -0,0 +1,34 @@
+with open('input.txt', 'r') as file:
+    t = ''
+    r = ''
+    for index, line in enumerate(file):
+        if index == 0:
+            t = line
+        if index == 1:
+            r = line
+    t = t[t.index(':') + 1:]
+    r = r[r.index(':') + 1:]
+    times = t.split()
+    records = r.split()
+
+    times_p2 = t.replace(' ', '')
+    records_p2 = r.replace(' ', '')
+
+tr_dict = {times[i]: records[i] for i in range(len(times))}
+
+
+def p1(T, R):
+    tmp_t = T
+    possible_times = []
+    for time in range(0, tmp_t):
+        d = time * (T - time)
+        if d > R:
+            possible_times.append(1)
+    return len(possible_times)
+
+
+out = 1
+for k, v in tr_dict.items():
+    out *= p1(int(k), int(v))
+print(f'part 1 answer: {out}')
+print(f'part 2 answer: {p1(int(times_p2), int(records_p2))}')
